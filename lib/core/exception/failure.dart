6723b31dc91e26ba8f81867d5a79e5e0e5f4348a
Semion <137element@gmail.com>
2024-11-17 17:40:35 +0800

    кастомная ошибка

---
+++ lib/core/exception/failure.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class Failure with EquatableMixin implements Exception{
+ final String message;
+ final int? statusCode;
+ final Exception exception;
+ final StackTrace stackTrace;
+
+  Failure({required this.message,  this.statusCode, required this.exception,  this.stackTrace=StackTrace.empty});
+
+  @override
+  List<dynamic> get props => [message,stackTrace,statusCode,exception];
+
+ @override
+  String toString() {
+    return 'Failure{message: $message, statusCode: $statusCode, exception: $exception, stackTrace: $stackTrace}';
+ }
+}
